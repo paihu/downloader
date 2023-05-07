e81ce937846f7f92ade7e66cb1086b842572751b
paihu <paihu_j@yahoo.co.jp>
2023-05-07 18:58:22 +0900

    fix wait

--- src/routes.ts
+++ src/routes.ts
@@ -14,16 +14,25 @@ router.addDefaultHandler(async ({ page, enqueueLinks, log }) => {
 });
 
 router.addHandler("detail", async ({ request, page, log }) => {
-  page.setDefaultTimeout(1000);
   // picture size 722 x 1024
   await page.setViewportSize({ width: 1444, height: 1024 });
 
+  // wait page loading
+  log.debug(`wait page loading...`);
+  await page.locator("#page-1").waitFor({ state: "attached" });
+
+  // wait close page layout notice
+  log.debug(`wait close page layout notice...`);
+  await page
+    .locator("span", { hasText: /(縦|横)読み/ })
+    .waitFor({ state: "detached" });
+
+  // when menu is open, close it
+  log.debug(`when menu is open, close it...`);
   const target = page.locator(".fixed.top-0.left-0.w-screen");
   if (await target.isVisible()) {
-    await page
-      .locator("div.h-full")
-      .first()
-      .click({ position: { x: 500, y: 722 } });
+    // click page center
+    await page.mouse.click(722, 500, { delay: 50 });
     await target.waitFor({ state: "detached" });
   }
 
@@ -32,7 +41,6 @@ router.addHandler("detail", async ({ request, page, log }) => {
   const seriesTitle = title.split("|")[1].split("-")[0].trim();
   const stories = request.loadedUrl?.split("/").pop() ?? "";
   log.info(`${seriesTitle}/${stories}-${subTitle}`, { url: request.loadedUrl });
-  await sleep(5000);
 
   const directory = `${process.argv[2]}-${seriesTitle}/${stories}-${subTitle}`;
 
@@ -42,7 +50,12 @@ router.addHandler("detail", async ({ request, page, log }) => {
         await mkdir(directory, { recursive: true });
       }
       if (!existsSync(`${directory}/page-${i}.png`)) {
-        const bgUrl = await page.locator(`#page-${i}`);
+        const bgUrl = page.locator(`#page-${i}`);
+        const loading = bgUrl.locator("div");
+        if (await loading.isVisible()) {
+          log.debug(`wait image loading...`);
+          await loading.waitFor({ state: "detached" });
+        }
         const buf = await bgUrl.screenshot();
         await writeFile(`${directory}/page-${i}.png`, buf);
       }
