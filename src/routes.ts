deb63389ec8ce35213b015a9a884bba219e008a7
paihu <paihu_j@yahoo.co.jp>
2023-05-07 17:19:17 +0900

    fix wait detach menu

--- src/routes.ts
+++ src/routes.ts
@@ -16,8 +16,17 @@ router.addDefaultHandler(async ({ page, enqueueLinks, log }) => {
 router.addHandler("detail", async ({ request, page, log }) => {
   page.setDefaultTimeout(1000);
   // picture size 722 x 1024
-  // header height 64px footer height 84px
-  await page.setViewportSize({ width: 1444, height: 1188 });
+  await page.setViewportSize({ width: 1444, height: 1024 });
+
+  const target = page.locator(".fixed.top-0.left-0.w-screen");
+  if (await target.isVisible()) {
+    await page
+      .locator("div.h-full")
+      .first()
+      .click({ position: { x: 500, y: 722 } });
+    await target.waitFor({ state: "detached" });
+  }
+
   const title = await page.title();
   const subTitle = title.split("|")[0].trim();
   const seriesTitle = title.split("|")[1].split("-")[0].trim();
@@ -25,21 +34,17 @@ router.addHandler("detail", async ({ request, page, log }) => {
   log.info(`${seriesTitle}/${stories}-${subTitle}`, { url: request.loadedUrl });
   await sleep(5000);
 
+  const directory = `${process.argv[2]}-${seriesTitle}/${stories}-${subTitle}`;
+
   try {
     for (let i = 0; true; i++) {
-      if (!existsSync(seriesTitle)) {
-        await mkdir(seriesTitle);
+      if (!existsSync(directory)) {
+        await mkdir(directory, { recursive: true });
       }
-      if (!existsSync(`${seriesTitle}/${stories}-${subTitle}`)) {
-        await mkdir(`${seriesTitle}/${stories}-${subTitle}`);
-      }
-      if (!existsSync(`${seriesTitle}/${stories}-${subTitle}/page-${i}.png`)) {
+      if (!existsSync(`${directory}/page-${i}.png`)) {
         const bgUrl = await page.locator(`#page-${i}`);
         const buf = await bgUrl.screenshot();
-        await writeFile(
-          `${seriesTitle}/${stories}-${subTitle}/page-${i}.png`,
-          buf
-        );
+        await writeFile(`${directory}/page-${i}.png`, buf);
       }
     }
   } catch (e) {
